29802d12de2c347912c6690be83a0e675264518f
4nt3ftw <ante.dubovecak@gmail.com>
2022-03-17 22:05:49 +0100

    Update gatsby-config.js
    graphql link to wp

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://localhost/gatsby-test/graphql`,
+          `http://wp-test.hv.digital/graphql`,
       },
     },
 
